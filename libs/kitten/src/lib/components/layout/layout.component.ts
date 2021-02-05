e7be14f5cee1b26a1a8fead1cdeb3064cfbd96f4
Alex Patterson <alex@ajonp.com>
2021-02-05 06:48:40 -0500

    add layout component

---
+++ libs/kitten/src/lib/components/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'codingcatdev-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/kitten/src/lib/kitten.module.ts
+++ libs/kitten/src/lib/kitten.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, LayoutComponent],
   exports: [ButtonComponent],
 })
 export class KittenModule {}
