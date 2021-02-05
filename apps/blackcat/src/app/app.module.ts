e3c994b516a99f87a43a78376585acbe533e0cbc
Alex Patterson <alex@ajonp.com>
2021-02-05 05:45:44 -0500

    tailwind working

--- apps/blackcat/src/app/app.module.ts
+++ apps/blackcat/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    prefix: '',
+    purge: {
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+};
