101ebf4ee62ac9677bb2475e09ac9ac0c5c62754
Alex Patterson <alex@ajonp.com>
2021-02-09 20:25:31 -0500

    add

--- backend/firebase/functions/src/ssr/ssr.ts
+++ backend/firebase/functions/src/ssr/ssr.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const universal = require(`./server/main`).app;
-export const ssr = functions.https.onRequest(universal);
+const universal = require(`${process.cwd()}/dist/blackcat/server/main`).app;
+// export const ssr = functions.https.onRequest(universal);
+export const ssr = functions.https.onRequest((req, res) => {
+  res.send(universal.toString());
+});
