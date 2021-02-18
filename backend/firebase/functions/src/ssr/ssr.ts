075feae0ed4c8d17239148b0db9bba381197718c
Alex Patterson <alex@ajonp.com>
2021-02-18 11:20:41 -0500

    remove firebase

--- backend/firebase/functions/src/ssr/ssr.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from 'firebase-functions';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const universal = require(`${process.cwd()}/dist/blackcat/server/main`).app;
-// export const ssr = functions.https.onRequest(universal);
-export const ssr = functions.https.onRequest((req, res) => {
-  res.send(universal.toString());
-});
